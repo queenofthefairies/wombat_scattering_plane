d2ce33834e1b0a626cfb25f338729d0d698d1cfe
queenofthefairies <siobhan.physics@gmail.com>
2025-10-17 15:45:05 +1100

    initial testing of openopt x ubmatrix code from william

---
+++ siobhan_scattering_plane_test.py
@@ -0,0 +1,44 @@
+# the virtual environment to run this script in is openopt-ubmatrix
+
+import sys
+import numpy as np
+
+# Add DerApproximator to path for openopt compatibility
+sys.path.append('J:/wombat_instrument_work/scattering_plane_software/OOSuite/DerApproximator')
+# Add openopt to path for openopt compatibility
+sys.path.append('J:/wombat_instrument_work/scattering_plane_software/OOSuite/OpenOpt/openopt')
+sys.path.append('J:/wombat_instrument_work/scattering_plane_software/OOSuite/OpenOpt')
+
+# Add UBmatrix to path
+sys.path.append('J:/wombat_instrument_work/scattering_plane_software/ubmatrix')
+
+#import oo as openopt
+#from openopt import NLSP
+from oo import NLSP
+import ubmatrix
+
+# Unit cell params for clinoatacamite
+clino_unit_cell_params = [6.144, 6.805, 9.112, 90.00, 99.55, 90.00]
+wavelength = 2.41 # in Angstrom
+# UB matrix found with Int3D
+clino_UBmatrix = np.array([[-0.04263322800398, 0.13622714579105, 0.02506458014250],
+                           [0.15944631397724, 0.03636400774121, 0.02596857771277],
+                           [0.00023406882246, 0.04140481352806, -0.10527275502682]])
+
+# calculate star (a.k.a. reciprocal lattice params)
+clino_star = ubmatrix.star(*clino_unit_cell_params)
+clino_star = dict(zip(('astar','bstar','cstar','alphastar','betastar','gammastar'),
+                         clino_star))
+print('clino star')
+print(clino_star)
+
+# calculate 2theta of a reflection 
+hkl = [-1,0,5]
+print('hkl: ({0}, {1}, {2})'.format(hkl[0],hkl[1],hkl[2]))
+print(ubmatrix.calcTwoTheta(hkl, clino_star, wavelength))
+
+# calculate scattering plane
+h1 = [0,0,1]
+h2 = [0,1,0]
+chi, phi = ubmatrix.calcScatteringPlane(h1, h2, clino_UBmatrix, wavelength,clino_star)
+print('chi: {0}    phi: {1}'.format(chi,phi))
