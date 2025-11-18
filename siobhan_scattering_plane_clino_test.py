edited calcIdealAngles2 function, changed to correct units (radians) for all angles

siobhan_scattering_plane_clino_test.py
@@ -47,27 +47,41 @@ hkl = [-1,0,5]
 print('hkl: ({0}, {1}, {2})'.format(hkl[0],hkl[1],hkl[2]))
 print(ubmatrix.calcTwoTheta(hkl, star, wavelength))
 
-wombat_scattering_plane.evaluate_possible_scattering_planes(sample_name_prefix, UB_matrix, wavelength, star)
+# have a look at which scattering planes are accessible given the UB matrix
+#wombat_scattering_plane.evaluate_possible_scattering_planes(sample_name_prefix, UB_matrix, wavelength, star)
 
-# # calculate scattering plane
-# h1 = [0,0,1]
-# h2 = [0,1,0]
-# chi, phi = ubmatrix.calcScatteringPlane(h1, h2, UB_matrix, wavelength, star)
-# print('chi: {0}    phi: {1}'.format(chi,phi))
+# for the hk0 scattering plane (which is apparently accessible), 
+# see which reflections are visible on Wombat given the Q limits
+# (+-3, +-3, 0) 
+# i.e. hkl_max_component_val = 3
+# plane_name = 'hk0'
+# hkl1 = [1, 0, 0]
+# hkl2 = [0, 1, 0]
+# hkl_max_component_val = 3
+# wom_stth = 13
+# wombat_scattering_plane.accessible_hkl_in_scattering_plane(sample_name_prefix, plane_name, 
+#                                                            hkl1, hkl2, hkl_max_component_val,
+#                                                            UB_matrix, wavelength, star, wom_stth)
 
-# # calculate scattering plane
-# h1 = [0,0,1]
-# h2 = [0,-1,0]
-# chi, phi = ubmatrix.calcScatteringPlane(h1, h2, UB_matrix, wavelength, star)
-# print('chi: {0}    phi: {1}'.format(chi,phi))
+# plane_name = 'hkk'
+# hkl1 = [1, 0, 0]
+# hkl2 = [0, 1, 1]
+# hkl_max_component_val = 3
+# wom_stth = 13
+# wombat_scattering_plane.accessible_hkl_in_scattering_plane(sample_name_prefix, plane_name, 
+#                                                            hkl1, hkl2, hkl_max_component_val,
+#                                                            UB_matrix, wavelength, star, wom_stth)
 
+# calculate angle between two hkls
+#interplanar_angle = np.arccos(d1 dot d2/ absd1 absd2)
+hkl1 = [1,2,2]
+hkl2 = [2,3,3]
+interplanar_angle = np.arccos(ubmatrix.scalar(*hkl1, *hkl2, star)/(ubmatrix.modvec(*hkl1, star)*ubmatrix.modvec(*hkl2, star)))*180/np.pi
+print('interplanar angle between ({0}, {1}, {2}) and ({3}, {4}, {5}): {6:.3f} degrees'.format(*hkl1, *hkl2, interplanar_angle))
 
-# # calculate scattering plane
-# h1 = [1,1,0]
-# h2 = [0,0,1]
-# chi, phi = ubmatrix.calcScatteringPlane(h1, h2, UB_matrix, wavelength, star)
-# print('chi: {0}    phi: {1}'.format(chi,phi))
-
-# # calculate ideal angles (with omega = 0)
-# twotheta, theta, omega, chi, phi = ubmatrix.calcIdealAngles(h1, UB_matrix, B_matrix, wavelength, star)
-# print('omega: {0}   chi: {1}    phi: {2}   2theta: {3}'.format(omega,chi,phi,twotheta))
+# calculate angle between two hkls
+#interplanar_angle = np.arccos(d1 dot d2/ absd1 absd2)
+hkl1 = [-1,2,2]
+hkl2 = [0,2,2]
+interplanar_angle = np.arccos(ubmatrix.scalar(*hkl1, *hkl2, star)/(ubmatrix.modvec(*hkl1, star)*ubmatrix.modvec(*hkl2, star)))*180/np.pi
+print('interplanar angle between ({0}, {1}, {2}) and ({3}, {4}, {5}): {6:.3f} degrees'.format(*hkl1, *hkl2, interplanar_angle))

wombat_scattering_plane.py
@@ -227,8 +227,8 @@ def accessible_hkl_in_scattering_plane(sample_name_prefix, plane_name, hkl1, hkl
     hkl2_array = np.array(hkl2)
 
     hkl_in_plane_to_test_list = []
-    for hkl1_component_val in range(-hkl_max_component_val, hkl_max_component_val):
-        for hkl2_component_val in range(-hkl_max_component_val, hkl_max_component_val):
+    for hkl1_component_val in range(-hkl_max_component_val, hkl_max_component_val+1):
+        for hkl2_component_val in range(-hkl_max_component_val, hkl_max_component_val+1):
             hkl_to_test = hkl1_component_val*hkl1_array + hkl2_component_val*hkl2_array
             hkl_in_plane_to_test_list.append(hkl_to_test)
 
@@ -236,7 +236,8 @@ def accessible_hkl_in_scattering_plane(sample_name_prefix, plane_name, hkl1, hkl
     hkl_in_plane_info_list = []
     for hkl in hkl_in_plane_to_test_list:
         hkl_list = hkl.tolist()
-        twotheta, theta, eom = ubmatrix.calcIdealAngles2(hkl_list, echi, ephi, UB_matrix, wavelength, stars)
+        twotheta, theta, eom = ubmatrix.calcIdealAngles2(hkl_list, echi, ephi, UB_matrix, wavelength, star)
+        #twotheta, theta, eom = ubmatrix.where_in_the_plane_is_my_hkl(hkl_list, echi, ephi, UB_matrix, wavelength, star)
         angle_accessible_bool = is_angle_accessible(twotheta, eom, echi, ephi, wom_stth)
         hkl_in_plane_info_list.append([*hkl_list, twotheta, eom, echi, ephi, wom_stth, angle_accessible_bool])
         if angle_accessible_bool == 0:
@@ -245,13 +246,13 @@ def accessible_hkl_in_scattering_plane(sample_name_prefix, plane_name, hkl1, hkl
             print('reflection {0} {1} {2} in {3} plane is accessible'.format(*hkl_list, plane_name))
 
     # save scattering plane info to excel spreadsheet
-    in_plane_hkl_df = pd.DataFrame(possible_planes_list, columns=['h1','k1','l1', 'twotheta', 'eom', 'echi','ephi', 'Wombat stth', 'Accessible?'])
+    in_plane_hkl_df = pd.DataFrame(hkl_in_plane_info_list, columns=['h1','k1','l1', 'twotheta', 'eom', 'echi','ephi', 'Wombat stth', 'Accessible?'])
     in_plane_hkl_df.to_excel('{0}_{1}_plane_hkl.xlsx'.format(sample_name_prefix, plane_name), index=False)
     print('\nIn-plane hkl info saved to {0}_{1}_plane_hkl.xlsx  \n'.format(sample_name_prefix, plane_name))
 
     # save accessible scattering planes to another spreadsheet
     accessible_in_plane_hkl_df = in_plane_hkl_df[in_plane_hkl_df['Accessible?'] == 1]
-    accessible_in_plane_hkl_df.to_excel('{0}_{1}_plane_accessible_hkl.xlsx'.format(sample_name_prefix, plane_name, index=False)
+    accessible_in_plane_hkl_df.to_excel('{0}_{1}_plane_accessible_hkl.xlsx'.format(sample_name_prefix, plane_name, index=False))
     print('\nAccessible in-plane hkl also saved to {0}_{1}_plane_accessible_hkl.xlsx  \n'.format(sample_name_prefix, plane_name))
 
     return